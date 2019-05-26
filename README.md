6cb89895b92d013eb6158633ad725cefc8e28766
gilda <gil@daganmail.com>
2019-05-26 23:11:47 +0300

    additional tests and README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+# BombaStompa
+BombaStompa is a timestamping service built on Ethereum.
+
+### Notes
 This is a project that is a timestamping service for files and data.
-MONOREPO
+This is a monorepo which includes both ethereum contracts as well as a web interface for BombaStompa.
--- test/ManagerTest.js
+++ test/ManagerTest.js
@@ -1,12 +1,45 @@
 const Manager = artifacts.require('./Manager.sol')
+const truffleAssert = require('truffle-assertions');
 
+// tests for the manager contract
 contract("Manager Hash Table", async accounts => {
+
+    // reset contract before each test
+    beforeEach(async () => {
+        manager = await Manager.new()
+    });
     
-    it("Should assign manager address at constructor", async () => {
-        let manager = await Manager.deployed();
+    it("Should assign manager address at constructor", async () => {        
+        // get manager
         let address = await manager.manager();
+        
         assert.equal(address, accounts[0]);
     });
 
-    
-})
+    it("Should store hash and retrieve the same", async () => {
+        // constants for testing
+        let hash = web3.utils.keccak256("file");
+        let name = "gilda";
+
+        // send and retrieve hash
+        await manager.addHash(name, hash);
+        let hashTest = await manager.getHash(name);
+        
+        assert.equal(hash, '0x' + hashTest.toString(16));
+    });
+
+    // error if entry already exists
+    it("Should throw if entry already exists", async () => {
+        // constants for testing
+        let hash = web3.utils.keccak256("file");
+        let hash1 = web3.utils.keccak256("file1");
+        let name = "gilda";
+
+        // send first hash
+        await manager.addHash(name, hash);
+
+        // send second hash (should throw)
+        await truffleAssert.reverts(manager.addHash(name, hash1));
+    });
+
+});
